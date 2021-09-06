5d348a90f2e520aa6c5c5241b22c32ee072755a7
mr.pan <1475148345@qq.com>
2021-09-06 16:13:04 +0800

    Update myController.js

--- admin/controller/myController.js
+++ admin/controller/myController.js
@@ -16,24 +16,7 @@ let myController={
     },
      /* 获取短信验证码 */
     getSMS:function(params,callback){
-        // const accessKeyId = ' ';
-        // const secretAccessKey = ' ';
-        let smsClient = new SMSClient({accessKeyId, secretAccessKey});
-        smsClient.sendSMS({
-            PhoneNumbers: params.phone,
-            SignName: '云通信产品',
-            TemplateCode: 'SMS_137730187',
-            TemplateParam: '{"code":"12345"}'
-        }).then(function (res) {
-            let {Code}=res
-            if (Code === 'OK') {
-                //处理返回参数
-                console.log(res)
-                callback(res)
-            }
-        }, function (err) {
-            console.log(err)
-        })
+        
     },
 };
 module.exports=myController;
